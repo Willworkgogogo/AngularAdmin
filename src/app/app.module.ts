53c72c06f51d74d2e52c54c109030438313e8b45
wangweiwei <will_workhard@163.com>
2018-01-11 00:09:20 +0800

    引入阿里antModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // antModule 第一步
 import { NgModule } from '@angular/core';
-
+import { NgZorroAntdModule } from 'ng-zorro-antd' // antModule 第二步
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
